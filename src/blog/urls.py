7469502031e2a64c52f45e75324e77f467d8863c
a.sotnichenko <haliivi@mail.ru>
2023-04-03 10:17:17 +0700

    1) Create aside.html, comments.html and post_detail.html templates. 2) Bind url to button in index.html. 3) Added url pattern for post detail. 4) Write PostDetailView class.

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,11 @@
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from .models import *
 
 __all__ = [
-    'IndexView'
+    'IndexView',
+    'PostDetailView',
 ]
 
 
@@ -17,4 +19,15 @@ class IndexView(TemplateView):
         page_number = self.request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context['page_obj'] = page_obj
+        return context
+
+
+class PostDetailView(TemplateView):
+    template_name = 'blog/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = kwargs.get('slug')
+        post = get_object_or_404(Post, url=slug)
+        context['post'] = post
         return context
